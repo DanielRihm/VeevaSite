f3c9d8e913150612e27a6ca0fe4e186c450717de
Daniel Rihm <rihm.7@osu.edu>
2021-11-06 23:05:53 -0400

    implemented new data collection methods

--- scripts/futuretopdoc.js
+++ scripts/futuretopdoc.js
@@ -1,49 +1,47 @@
-$(function () {
-    $.get('/Prescriber_Data.csv', function (data) {
-        // create input seperated by line
-        var input = data.split('\r\n');
+var data = sessionStorage.getItem("csvInput");
 
-        // create table of all of the data seperated out
-        var columnData = parseCSV(input);
+// create input seperated by line
+var input = data.split('\n');
 
-        // create array of doctor prescription predictions
-        var docPredict = [];
+// create table of all of the data seperated out
+var columnData = parseCSV(input);
 
-        // populate array with data
-        for (var i = 0; i < columnData.length; i++) {
-            var futureDoc = predictDoctor(columnData[i]);
-            findFutureTopDoctor(docPredict, futureDoc);
-        }
+// create array of doctor prescription predictions
+var docPredict = [];
 
-        //define data array
-        var tabledata = [];
-        for (var i = 0; i < docPredict.length; i++) {
-            tabledata[i] = {
-                id:i+1,
-                name:docPredict[i].name,
-                count:docPredict[i].futureTotal,
-                product:docPredict[i].product
-            };
-        }
+// populate array with data
+for (var i = 0; i < columnData.length; i++) {
+    var futureDoc = predictDoctor(columnData[i]);
+    findFutureTopDoctor(docPredict, futureDoc);
+}
 
-        var table = new Tabulator("#FutureTopDocTable", {
-            data:tabledata,           //load row data from array
-            layout:"fitColumns",      //fit columns to width of table
-            responsiveLayout:"hide",  //hide columns that dont fit on the table
-            addRowPos:"top",          //when adding a new row, add it to the top of the table
-            history:true,             //allow undo and redo actions on the table
-            pagination:"local",       //paginate the data
-            paginationSize:7,         //allow 7 rows per page of data
-            movableColumns:true,      //allow column order to be changed
-            resizableRows:true,       //allow row order to be changed
-            initialSort:[             //set the initial sort order of the data
-                {column:"name", dir:"asc"},
-            ],
-            columns:[                 //define the table columns
-                {title:"Doctor", field:"name", editor:"input"},
-                {title:"Predicted Total Prescriptions", field:"count", editor:"input"},
-                {title:"Product", field:"product", editor:"input"}
-            ],
-        });
-    });
+//define data array
+var tabledata = [];
+for (var i = 0; i < docPredict.length; i++) {
+    tabledata[i] = {
+        id:i+1,
+        name:docPredict[i].name,
+        count:docPredict[i].futureTotal,
+        product:docPredict[i].product
+    };
+}
+
+var table = new Tabulator("#FutureTopDocTable", {
+    data:tabledata,           //load row data from array
+    layout:"fitColumns",      //fit columns to width of table
+    responsiveLayout:"hide",  //hide columns that dont fit on the table
+    addRowPos:"top",          //when adding a new row, add it to the top of the table
+    history:true,             //allow undo and redo actions on the table
+    pagination:"local",       //paginate the data
+    paginationSize:7,         //allow 7 rows per page of data
+    movableColumns:true,      //allow column order to be changed
+    resizableRows:true,       //allow row order to be changed
+    initialSort:[             //set the initial sort order of the data
+        {column:"name", dir:"asc"},
+    ],
+    columns:[                 //define the table columns
+        {title:"Doctor", field:"name", editor:"input"},
+        {title:"Predicted Total Prescriptions", field:"count", editor:"input"},
+        {title:"Product", field:"product", editor:"input"}
+    ],
 });
--- scripts/topdoctable.js
+++ scripts/topdoctable.js
@@ -1,48 +1,46 @@
-$(function () {
-    $.get('/Prescriber_Data.csv', function (data) {
-        // create input seperated by line
-        var input = data.split('\r\n');
+var data = sessionStorage.getItem("csvInput");
 
-        // create table of all of the data seperated out
-        var columnData = parseCSV(input);
+// create input seperated by line
+var input = data.split('\n');
 
-        // declare maximum object array for the products
-        var maxProduct = [];
+// create table of all of the data seperated out
+var columnData = parseCSV(input);
 
-        // find the max products
-        for (var i = 0; i < columnData.length; i++) {
-            findMaxPresciption(columnData[i],maxProduct);
-        }
+// declare maximum object array for the products
+var maxProduct = [];
 
-        //define data array
-        var tabledata = [];
-        for (var i = 0; i < maxProduct.length; i++) {
-            tabledata[i] = {
-                id:i+1,
-                product:maxProduct[i].product,
-                count:maxProduct[i].count,
-                name:maxProduct[i].name
-            };
-        }
+// find the max products
+for (var i = 0; i < columnData.length; i++) {
+    findMaxPresciption(columnData[i],maxProduct);
+}
 
-        var table = new Tabulator("#TopDocTable", {
-            data:tabledata,           //load row data from array
-            layout:"fitColumns",      //fit columns to width of table
-            responsiveLayout:"hide",  //hide columns that dont fit on the table
-            addRowPos:"top",          //when adding a new row, add it to the top of the table
-            history:true,             //allow undo and redo actions on the table
-            pagination:"local",       //paginate the data
-            paginationSize:7,         //allow 7 rows per page of data
-            movableColumns:true,      //allow column order to be changed
-            resizableRows:true,       //allow row order to be changed
-            initialSort:[             //set the initial sort order of the data
-                {column:"name", dir:"asc"},
-            ],
-            columns:[                 //define the table columns
-                {title:"Doctor", field:"name", editor:"input"},
-                {title:"Total Prescribed", field:"count", editor:"input"},
-                {title:"Product", field:"product", editor:"input"}
-            ],
-        });
-    });
+//define data array
+var tabledata = [];
+for (var i = 0; i < maxProduct.length; i++) {
+    tabledata[i] = {
+        id:i+1,
+        product:maxProduct[i].product,
+        count:maxProduct[i].count,
+        name:maxProduct[i].name
+    };
+}
+
+var table = new Tabulator("#TopDocTable", {
+    data:tabledata,           //load row data from array
+    layout:"fitColumns",      //fit columns to width of table
+    responsiveLayout:"hide",  //hide columns that dont fit on the table
+    addRowPos:"top",          //when adding a new row, add it to the top of the table
+    history:true,             //allow undo and redo actions on the table
+    pagination:"local",       //paginate the data
+    paginationSize:7,         //allow 7 rows per page of data
+    movableColumns:true,      //allow column order to be changed
+    resizableRows:true,       //allow row order to be changed
+    initialSort:[             //set the initial sort order of the data
+        {column:"name", dir:"asc"},
+    ],
+    columns:[                 //define the table columns
+        {title:"Doctor", field:"name", editor:"input"},
+        {title:"Total Prescribed", field:"count", editor:"input"},
+        {title:"Product", field:"product", editor:"input"}
+    ],
 });
